25b020cea7507e27ec2f9a4187285b10fc2194e7
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-04-05 16:42:10 +0200

    admin.py in chat and chatbot cleared

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import ChatMessage, FAQ, Tenant, Conversation
 
 
-@admin.register(Tenant)
-class TenantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner_email', 'widget_position', 'widget_color', 'widget_title')
-
-
-admin.site.register(FAQ)
-admin.site.register(Conversation)
-admin.site.register(ChatMessage)
+# @admin.register(Tenant)
+# class TenantAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'owner_email', 'widget_position', 'widget_color', 'widget_title')
+#
+#
+# admin.site.register(FAQ)
+# admin.site.register(Conversation)
+# admin.site.register(ChatMessage)
