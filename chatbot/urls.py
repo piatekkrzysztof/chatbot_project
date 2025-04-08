e68162dbf435cefd5ac19f449d31f567c4c808d4
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-04-08 13:45:11 +0200

    chatbot deleted

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-# from . import views
-#
-# urlpatterns=[
-#     path('chat/', views.chat_with_gpt, name="chat_with_gpt"),
-#     path('api/widget-settings/', views.widget_settings, name='widget-settings'),
-# ]
