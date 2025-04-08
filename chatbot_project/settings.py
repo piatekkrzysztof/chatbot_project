09ccf26779b617b8ba5aafc676a074d2b96cd4fa
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-04-08 13:52:48 +0200

    chatbot deleted from settimgs

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chatbot',
     'rest_framework',
     'accounts',
     'chat',
