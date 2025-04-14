f073a1131a9061263c9a42959ba471051f122b0d
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-04-14 15:25:08 +0200

    tests and db fixed

--- conftest.py
+++ conftest.py
@@ -1,14 +1,28 @@
 import pytest
+import os
 from dotenv import load_dotenv
+from django.conf import settings
 from django.db import connection
+from django.core.management import call_command
 
-# Załaduj plik .env.test przed testami
 load_dotenv(".env.test")
 
 
-# Dodaj rozszerzenie pgvector do bazy testowej
+@pytest.fixture(scope="session")
+def django_db_setup():
+    settings.DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ["PGPORT"],
+    }
+
+
 @pytest.fixture(scope="session", autouse=True)
-def ensure_pgvector_installed(django_db_setup, django_db_blocker):
+def setup_db_and_vector(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         with connection.cursor() as cursor:
             cursor.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+        call_command("migrate")
