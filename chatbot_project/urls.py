f6b75b19932d1b6170413fe431d11f345f9e9a34
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-04-29 20:01:26 +0200

    urls update

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -30,7 +30,7 @@ def health_check(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('health/', health_check),  # <= dodany health-check
+    path('health/', health_check),
 ]
 
-urlpatterns += static('/static/', document_root=os.path.join(settings.BASE_DIR, 'widget', 'static'))
+# urlpatterns += static('/static/', document_root=os.path.join(settings.BASE_DIR, 'widget', 'static'))
