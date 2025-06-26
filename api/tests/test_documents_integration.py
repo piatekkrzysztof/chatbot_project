9eace54de1dcf2ee9a4b25fa3db35e37dbeac33b
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-06-26 11:39:05 +0200

    test_documents_integration

--- api/tests/test_documents_integration.py
+++ api/tests/test_documents_integration.py
@@ -1,10 +1,18 @@
 import pytest
 from documents.models import Document
 from unittest.mock import patch
+from rest_framework.test import APIClient
 
 @pytest.mark.django_db
 @patch("api.views.documents.embed_document_task.delay")
-def test_document_upload_dispatches_embedding_task(mock_embedding_task, api_client, tenant, valid_pdf_file):
+def test_document_upload_dispatches_embedding_task(mock_embedding_task, api_client, tenant,user, valid_pdf_file,subscribtion):
+    client = APIClient()
+    user.tenant = tenant
+    user.role = "owner"
+    user.save()
+    tenant.save()
+    client.force_authenticate(user=user)
+
     response = api_client.post(
         "/api/documents-upload/",
         {
@@ -12,7 +20,7 @@ def test_document_upload_dispatches_embedding_task(mock_embedding_task, api_clie
             "file": valid_pdf_file
         },
         format="multipart",
-        HTTP_X_API_KEY=tenant.api_key
+        HTTP_X_API_KEY=str(tenant.api_key)
     )
 
     assert response.status_code == 201
