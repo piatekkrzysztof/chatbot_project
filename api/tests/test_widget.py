337ddcdacd96452bbd43b00a55ea5c32e2fa3596
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2025-06-26 17:35:28 +0200

    test widget

--- api/tests/test_widget.py
+++ api/tests/test_widget.py
@@ -5,19 +5,16 @@ import uuid
 
 
 @pytest.mark.django_db
-def test_widget_settings_success():
-    client = APIClient()
-    api_key = uuid.uuid4()
-    tenant = Tenant.objects.create(
-        name="Testowa Firma",
-        api_key=api_key,
-        widget_position="right",
-        widget_color="#00ff00",
-        widget_title="Zapytaj nas!",
-        owner_email="owner@example.com"
-    )
+def test_widget_settings_success(api_client, tenant, user, subscribtion, ):
+    tenant.widget_position = "right",
+    tenant.widget_color = "#00ff00",
+    tenant.widget_title = "Zapytaj nas!"
+    user.tenant = tenant
+    user.role = "owner"
+    user.save()
+    api_client.force_authenticate(user=user)
 
-    response = client.get("/api/widget/settings/", HTTP_X_API_KEY=str(api_key))
+    response = api_client.get("/api/widget-settings/", HTTP_X_API_KEY=str(tenant.api_key))
 
     assert response.status_code == 200
     assert response.json() == {
@@ -28,7 +25,7 @@ def test_widget_settings_success():
 
 
 @pytest.mark.django_db
-def test_widget_settings_invalid_key():
+def test_widget_settings_invalid_key(api_client, tenant, user, subscribtion, ):
     client = APIClient()
     response = client.get("/api/widget/settings/", HTTP_X_API_KEY=str(uuid.uuid4()))
     assert response.status_code == 403
@@ -36,15 +33,15 @@ def test_widget_settings_invalid_key():
 
 
 @pytest.mark.django_db
-def test_widget_settings_missing_key():
+def test_widget_settings_missing_key(api_client, tenant, user, subscribtion, ):
     client = APIClient()
     response = client.get("/api/widget/settings/")
     assert response.status_code == 400
-    assert "error" in response.json()
+    assert "error" in response.json(api_client, tenant, user, subscribtion, )
 
 
 @pytest.mark.django_db
-def test_widget_settings_invalid_uuid_format():
+def test_widget_settings_invalid_uuid_format(api_client, tenant, user, subscribtion, ):
     client = APIClient()
     response = client.get("/api/widget/settings/", HTTP_X_API_KEY="not-a-uuid")
     assert response.status_code == 400
